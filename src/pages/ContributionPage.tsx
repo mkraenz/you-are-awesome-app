b2bf814c2a44345e0e85d02d3ecd5f2f39142083
Mirco Kraenz <kraenzmirco@gmail.com>
2019-10-13 13:45:44 +0200

    UPDATE #1 under development message for sending posts

--- src/pages/ContributionPage.tsx
+++ src/pages/ContributionPage.tsx
@@ -49,10 +49,12 @@ class ContributionPage extends Component<Props> {
             ...post,
             id: uuid.v4(),
         });
+        const sorry =
+            "\n\nUnfortunately, we cannot save your contributions on the server yet because the app is still in early development. We are working on it and keep you updated!";
         // TODO #2 show spinner until the POST request was successful (or failed)
         Alert.alert(
             "Thanks for your contribution!",
-            `You are a valued member of our awesome community. \n Your message: \n ${post.text}`,
+            `You are a valued member of our awesome community. \n Your message: \n ${post.text}${sorry}`,
             [
                 {
                     text: "Awesome!",
