3d7c9ba03edf1818e94cd6094517249ce550e9c6
Mirco Kraenz <kraenzmirco@gmail.com>
2019-10-19 22:08:04 +0200

    REFACTOR #12 rename postAdded -> postSendRequested leftover

--- src/redux/sagas/sendPostSaga.ts
+++ src/redux/sagas/sendPostSaga.ts
@@ -1,15 +1,15 @@
 import { call, put, select, takeEvery } from "redux-saga/effects";
+import { waitAndSendPostToServer } from "../../api/sendPostToServer";
 import { MAX_BACKOFF_IN_MS } from "../../config";
 import { IPostSendFailed, IPostSendRequested } from "../Actions";
 import { SEND_POST_URI } from "../reducers/postReducer";
 import { ReduxAction } from "../ReduxAction";
 import { backoffInMs } from "../selectors";
-import { waitAndSendPostToServer } from "../../api/sendPostToServer";
 
 const FINAL_ERROR_MSG = "Maximum timeout exceeded. Sending to server failed.";
 
 function* sendPostWorkerSaga(action: IPostSendRequested | IPostSendFailed) {
-    const postAdded =
+    const postSendRequested =
         action.type === ReduxAction.PostSendRequested
             ? action
             : action.payload.originalAction;
@@ -20,7 +20,7 @@ function* sendPostWorkerSaga(action: IPostSendRequested | IPostSendFailed) {
         }
         const responseData = yield call(
             waitAndSendPostToServer,
-            postAdded.payload,
+            postSendRequested.payload,
             SEND_POST_URI,
             backoff
         );
@@ -29,22 +29,22 @@ function* sendPostWorkerSaga(action: IPostSendRequested | IPostSendFailed) {
             payload: responseData,
         });
     } catch (e) {
-        yield* handleSendFailed(e, postAdded);
+        yield* handleSendFailed(e, postSendRequested);
     }
 }
 
-function* handleSendFailed(e: any, postAdded: IPostSendRequested) {
+function* handleSendFailed(e: any, action: IPostSendRequested) {
     if (e.message === FINAL_ERROR_MSG) {
         const finalErrorAction = {
             type: ReduxAction.PostSendFailedTimeoutExceeded,
-            payload: { originalAction: postAdded, error: e },
+            payload: { originalAction: action, error: e },
             error: true,
         };
         yield put(finalErrorAction);
     } else {
         const errorAction: IPostSendFailed = {
             type: ReduxAction.PostSendFailed,
-            payload: { originalAction: postAdded, error: e },
+            payload: { originalAction: action, error: e },
             error: true,
         };
         yield put(errorAction);
