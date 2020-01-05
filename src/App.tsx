6f13f2f7cae8b8524c59be7b20bef767d57faa1d
Mirco Kraenz <kraenzmirco@gmail.com>
2020-01-05 10:00:00 +0100

    REFACTOR #88 containers use props.children over hardcoding

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Provider } from "react-redux";
 import AppStateChangedContainer from "./AppStateChangedContainer";
+import NavigationAppContainer from "./NavigationAppContainer";
 import NetInfoChangedContainer from "./NetInfoChangedContainer";
 import store from "./redux";
 
 const App = () => (
     <Provider store={store}>
         <AppStateChangedContainer>
-            <NetInfoChangedContainer />
+            <NetInfoChangedContainer>
+                <NavigationAppContainer />
+            </NetInfoChangedContainer>
         </AppStateChangedContainer>
     </Provider>
 );
