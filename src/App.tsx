45bdff20675e54740ece441ab97fa50ecc02506b
Mirco Kraenz <kraenzmirco@gmail.com>
2020-01-07 22:03:36 +0100

    REFACTOR #91 #88 use props.children over hardcoding

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { Provider } from "react-redux";
 import AppStateChangedContainer from "./AppStateChangedContainer";
+import NavigationAppContainer from "./NavigationAppContainer";
+import NetInfoChangedContainer from "./NetInfoChangedContainer";
 import store from "./redux";
 
 const App = () => (
     <Provider store={store}>
-        <AppStateChangedContainer />
+        <AppStateChangedContainer>
+            <NetInfoChangedContainer>
+                <NavigationAppContainer />
+            </NetInfoChangedContainer>
+        </AppStateChangedContainer>
     </Provider>
 );
 
