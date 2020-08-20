63afc8736da76be20e4d75214e50b930e649f9e1
Mirco Kraenz <kraenzmirco@gmail.com>
2020-08-20 23:35:21 +0900

    CONFIG #220 publish to prod

--- scripts/expo-publish-with-version.js
+++ scripts/expo-publish-with-version.js
@@ -6,4 +6,4 @@ const data = jsonfile.readFileSync(file);
 data.jsBuildNumber = data.jsBuildNumber + 1;
 
 jsonfile.writeFileSync(file, data, { spaces: 2, EOL: "\r\n" });
-shell.exec("expo publish");
+shell.exec("expo publish --target bare");
