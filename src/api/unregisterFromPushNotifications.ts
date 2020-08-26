6b83a281cd854feac6d814d267386b3f125b8914
Mirco Kraenz <kraenzmirco@gmail.com>
2020-08-26 22:58:38 +0900

    REFACTOR #244 use axios for push notification

--- src/api/unregisterFromPushNotifications.ts
+++ src/api/unregisterFromPushNotifications.ts
@@ -1,16 +1,19 @@
+import axios from "axios";
 import * as Notifications from "expo-notifications";
 import { URI } from "../config";
 
 export const unregisterFromPushNotifications = async () => {
     let token = await Notifications.getExpoPushTokenAsync();
-    return fetch(URI.UNREGISTER_PUSH_NOTIFICATION, {
-        method: "POST",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
+    await axios.post(
+        URI.UNREGISTER_PUSH_NOTIFICATION,
+        {
             token: token.data,
-        }),
-    });
+        },
+        {
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+        }
+    );
 };
