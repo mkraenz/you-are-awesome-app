347b664d3f58d45364cf6bb8dea35d650a039d03
Mirco Kraenz <kraenzmirco@gmail.com>
2020-08-29 19:51:30 +0900

    CLEANUP #254 remains of backoff

--- __tests__/store/reducers/submitMessageReducer.test.ts
+++ __tests__/store/reducers/submitMessageReducer.test.ts
@@ -1,6 +1,6 @@
 import { ActionType } from "../../../src/state/actions/ActionType";
 import {
-    ISubmitMessageFailed,
+    ISubmitMessageRequested,
     ISubmitMessageSucceeded,
 } from "../../../src/state/actions/SubmitMessageAction";
 import { submitMessageReducer } from "../../../src/state/reducers/submitMessageReducer";
@@ -32,18 +32,13 @@ describe("submitMessageReducer", () => {
         expect(result).toEqual(expected);
     });
 
-    describe(`${ActionType.SubmitMessageFailed}`, () => {
-        it("should backoff 1 sec if first fail", () => {
-            const action: ISubmitMessageFailed = {
-                type: ActionType.SubmitMessageFailed,
+    describe(`${ActionType.SubmitMessageRequested}`, () => {
+        it("should add the new message to my messages", () => {
+            const action: ISubmitMessageRequested = {
+                type: ActionType.SubmitMessageRequested,
                 payload: {
-                    error: new Error("an error message"),
-                    originalAction: {
-                        type: ActionType.SubmitMessageRequested,
-                        payload: mock.message,
-                    },
+                    ...mock.message,
                 },
-                error: true,
             };
             const state: ISubmitMessageState = {
                 myMessages: [],
@@ -52,7 +47,7 @@ describe("submitMessageReducer", () => {
             const result = submitMessageReducer(state, action);
 
             const expected: ISubmitMessageState = {
-                myMessages: [],
+                myMessages: [mock.message],
             };
             expect(result).toEqual(expected);
         });
--- src/state/reducers/submitMessageReducer.ts
+++ src/state/reducers/submitMessageReducer.ts
@@ -3,7 +3,6 @@ import { ActionType } from "../actions/ActionType";
 import { SubmitMessageAction } from "../actions/SubmitMessageAction";
 import { ISubmitMessageState } from "../state/ISubmitMessageState";
 
-/** exponential backoff for submitting a new message to the server */
 export const submitMessageReducer: Reducer<
     ISubmitMessageState,
     SubmitMessageAction
@@ -19,10 +18,6 @@ export const submitMessageReducer: Reducer<
                 ...state,
                 myMessages: [...state.myMessages, action.payload],
             };
-        case ActionType.SubmitMessageSucceeded:
-            return { ...state };
-        case ActionType.SubmitMessageFailed:
-            return { ...state };
         default:
             return state;
     }
