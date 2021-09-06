b34c3f69cf004bd0e20fa45a84619dd0169e34ce
Mirco Kraenz <kraenzmirco@gmail.com>
2021-09-06 23:02:07 +0200

    ci(#543): fix changelog generation

--- .releaserc.js
+++ .releaserc.js
@@ -3,7 +3,8 @@ module.exports = {
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
-        "@semantic-release/changelog",
+        ["@semantic-release/changelog", { changelogFile: "CHANGELOG.md" }],
+        ["@semantic-release/git", { assets: ["CHANGELOG.md"] }],
         "@semantic-release/github",
     ],
 };
