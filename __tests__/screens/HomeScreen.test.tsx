e5f98f9aa6c2650acc0fed056f656e95ddb9fd07
Mirco Kraenz <kraenzmirco@gmail.com>
2021-09-08 11:56:15 +0200

    test(#541): double tap to like, fetch on open of home screen

--- __tests__/screens/HomeScreen.test.tsx
+++ __tests__/screens/HomeScreen.test.tsx
@@ -1,13 +1,19 @@
 import { fireEvent, render } from "@testing-library/react-native";
 import Clipboard from "expo-clipboard";
 import i18next from "i18next";
+import MockDate from "mockdate";
 import React from "react";
 import "react-native";
+import {
+    LongPressGestureHandler,
+    TapGestureHandler,
+} from "react-native-gesture-handler";
 import { Provider } from "react-redux";
 // Note: test renderer must be required after react-native.
 import renderer from "react-test-renderer";
 import createMockStore from "redux-mock-store";
 import HomeScreen from "../../src/screens/HomeScreen";
+import { ActionType } from "../../src/state/actions/ActionType";
 import { IState } from "../../src/state/state/IState";
 import { Pick2 } from "../../src/utils/ts/Pick2";
 import LocalizedMockPaperProvider from "../helpers/LocalizedMockPaperProvider";
@@ -19,6 +25,10 @@ const ConfiguredHomeScreen = () => (
         <HomeScreen />
     </LocalizedMockPaperProvider>
 );
+
+afterEach(() => {
+    MockDate.reset();
+});
 
 it("renders correctly", () => {
     const store = createMockStore<
@@ -48,6 +58,41 @@ it("renders correctly", () => {
         .toJSON();
 
     expect(tree).toMatchSnapshot();
+});
+
+it("requests to fetch messages", async () => {
+    MockDate.set("2021-01-01T00:00:00.000Z");
+    const store = createMockStore<
+        Pick2<IState, "messages", "currentMessage"> &
+            Pick2<IState, "network", "connected">
+    >([])({
+        messages: {
+            currentMessage: {
+                id: "1",
+                author: "my-author",
+                text: "my-text",
+                country: "my-country",
+                isodate: "2018-08-20",
+            },
+        },
+        network: {
+            connected: true,
+        },
+    });
+
+    render(
+        <Provider store={store}>
+            <ConfiguredHomeScreen />
+        </Provider>
+    );
+    expect(store.getActions()).toEqual([
+        {
+            type: ActionType.FetchMessagesRequested,
+            payload: {
+                now: new Date("2021-01-01T00:00:00.000Z"),
+            },
+        },
+    ]);
 });
 
 it("opens the report dialog when clicking the flag button in the appbar", async () => {
@@ -143,21 +188,69 @@ it("copies the message to clipboard and notifies the user on long press", async 
         },
     });
 
-    const { findByText, queryByText, debug, getByA11yLabel, getByText } =
-        render(
-            <Provider store={store}>
-                <ConfiguredHomeScreen />
-            </Provider>
-        );
+    const { getByA11yLabel, getByText } = render(
+        <Provider store={store}>
+            <ConfiguredHomeScreen />
+        </Provider>
+    );
     const quoteView = getByA11yLabel(
         "long press to copy message, double tap to add to favorites"
     );
+    const longPressGestureHandler = quoteView.parent!.parent!;
 
-    const longPressGestureHandler = quoteView.parent!;
+    // simulates successful long press
     fireEvent(longPressGestureHandler, "onActivated");
 
+    expect(longPressGestureHandler.type).toEqual(LongPressGestureHandler);
     expect(getByText(i18next.t("home:copiedInfo"))).toBeTruthy();
     expect(clipboardSpy).toHaveBeenCalledWith(
         "my-text - my-author from my-country - via You are Awesome App!"
     );
+});
+
+it("adds the message to favorites on double tap", async () => {
+    jest.useFakeTimers();
+    const store = createMockStore<
+        Pick2<IState, "messages", "currentMessage"> &
+            Pick2<IState, "network", "connected">
+    >([])({
+        messages: {
+            currentMessage: {
+                id: "1",
+                author: "my-author",
+                text: "my-text",
+                country: "my-country",
+                isodate: "2018-08-20",
+            },
+        },
+        network: {
+            connected: true,
+        },
+    });
+
+    const { getByA11yLabel } = render(
+        <Provider store={store}>
+            <ConfiguredHomeScreen />
+        </Provider>
+    );
+    const quoteView = getByA11yLabel(
+        "long press to copy message, double tap to add to favorites"
+    );
+
+    const doubleTap = quoteView.parent!.parent!.parent!;
+
+    // simulates successful double tap
+    fireEvent(doubleTap, "onActivated");
+
+    expect(doubleTap.type).toEqual(TapGestureHandler);
+    expect(store.getActions()[1]).toEqual({
+        type: ActionType.AddToFavorites,
+        payload: {
+            id: "1",
+            author: "my-author",
+            text: "my-text",
+            country: "my-country",
+            isodate: "2018-08-20",
+        },
+    });
 });
